612d95b68ca1ba1ecf5c5a8f777bc3812a5faf30
carl sednaoui <carlsednaoui@gmail.com>
2012-03-19 23:20:25 +0100

    added url validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
 	validates_presence_of :title
 	validates_presence_of :url
+	validates_format_of :url, :with => URI::regexp(%w(http https)), :message => 'needs to begin with http:// or https://'
 end
