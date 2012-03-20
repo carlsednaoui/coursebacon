3caeab0b1928bc5af3d212bb8733ffcee6683a65
carl sednaoui <carlsednaoui@gmail.com>
2012-03-20 16:08:52 +0100

    testing to implement fb ssl fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'kivBXFB5gE7RunUmBXgoWg', 'O8PLhMwETapHwD55AuUL4K8e27XwDb1GZI4jtj4cwg'
-  provider :facebook, '271024399642949', '548830ca3066450af6e4e86703b209f9'
+  provider :facebook, '271024399642949', '548830ca3066450af6e4e86703b209f9', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
