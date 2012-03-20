957227806d273ee331981d4ff026aab6074bcece
carl sednaoui <carlsednaoui@gmail.com>
2012-03-20 01:24:22 +0100

    added course show syllabus NA

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
+
+Course.create(:title => 'course title', :url => 'http://www.thisisaurl.com', :offered_by => 'institution', :main_topic => 'rails', :description => 'descroiption')
+
+
 # Examples:
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
