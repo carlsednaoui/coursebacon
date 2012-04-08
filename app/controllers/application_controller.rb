c3c3c1c86c43d6e65700f9fe8d2e24abbdea79c5
carl sednaoui <carlsednaoui@gmail.com>
2012-04-08 13:40:57 -0400

    added stats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user
+  before_filter :get_stats
 
  private
+
+ def get_stats
+	@course_num = Course.count
+	@tutorial_num = Tutorial.count
+	@book_num = Book.count
+	@tweet_num = Tweet.count
+ end
 
   def current_user
 	    @current_user ||= User.find(session[:user_id]) if session[:user_id]
