87cf56c8b78a7e5e013f71b87c296c1a289108be
carl sednaoui <carlsednaoui@gmail.com>
2012-04-15 00:36:20 -0400

    worked on ranking algo

--- app/controllers/url_datas_controller.rb
+++ app/controllers/url_datas_controller.rb
@@ -1,5 +1,15 @@
 class UrlDatasController < ApplicationController
 def index
 	@data = UrlData.all
+
+	@backlinks = Array.new
+	@data.each do |details|
+		@backlinks.push details.google_backlinks
+		@backlinks.push details.moz_backlinks
+	end
+
+	@backlinks = @backlinks.compact.reject { |s| s.blank? }
+
+	@backlinks = @backlinks.inject(:+)
 end
 end
--- get_link_data.rb
+++ get_link_data.rb
@@ -17,7 +17,7 @@ puts "Courses count: " + @new_courses.count.to_s
 	
 	@url_data = UrlData.new
 	@url_data.course_id = course
-	@query = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=site:"
+	@query = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=link:"
 
 	#Get Google Backlinks
 	uri = URI("#{@query + url}")
@@ -25,7 +25,7 @@ puts "Courses count: " + @new_courses.count.to_s
 	parsed_json = ActiveSupport::JSON.decode(result)
 
 	puts "Start of Google Link Analysis"
-	@url_data.google_backlinks = parsed_json["responseData"]["cursor"]["estimatedResultCount"].to_s
+	@url_data.google_backlinks = parsed_json["responseData"]["cursor"]["estimatedResultCount"]
 	puts "Response Status: " + parsed_json["responseStatus"].to_s
 	puts "End of Google Link Analysis"
 
@@ -33,9 +33,14 @@ puts "Courses count: " + @new_courses.count.to_s
 	client = Linkscape::Client.new(:accessID => "member-e2f921a0d2", :secret => "5d5f04f85b6851260dbe25f788ac899a")
 	response = client.urlMetrics("#{url}", :cols => :all)
 
+	#If a URL has more than 1000 seomoz backlinks, set the number of backlinks to 1000 - this is done in order to normalize the data
 	puts "Start SEOMoz Details"
-	@url_data.moz_backlinks = response.data[:external_links].to_s
-	@url_data.mozrank = response.data[:mozrank].to_s
+	if response.data[:external_links] > 1000
+		@url_data.moz_backlinks = 1000
+	else
+		@url_data.moz_backlinks = response.data[:external_links]
+	end
+	@url_data.mozrank = response.data[:mozrank]
 	puts "End SEOMoz Details"
 
 	begin
