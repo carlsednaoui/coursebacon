aae6014b8b8bb8cf74da3bcac124dc31c3e2046b
carl sednaoui <carlsednaoui@gmail.com>
2012-04-15 15:53:42 -0400

    fixed bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,25 +6,37 @@ module ApplicationHelper
 		link_to title, {:sort => column, :direction => direction}, {:class => css_class}
 	end
 
-	def popular_course(course_id, backlinks = @backlinks) 
+	def popular_course(course_id) 
 		tweets = Tweet.find_all_by_course_id(course_id).count
-		links = UrlData.find_by_course_id(course_id).moz_backlinks.to_f + UrlData.find_by_course_id(course_id).google_backlinks.to_f
+		if UrlData.find_by_course_id(course_id).nil?
+			links = 0
+		else
+			links = UrlData.find_by_course_id(course_id).moz_backlinks.to_f + UrlData.find_by_course_id(course_id).google_backlinks.to_f
+		end
 		course_popularity = tweets + links
 		course_popularity = course_popularity.to_i
 		return course_popularity
 	end
 
-	def popular_tutorial(tutorial_id, backlinks = @backlinks)
+	def popular_tutorial(tutorial_id)
 		tweets = Tweet.find_all_by_tutorial_id(tutorial_id).count
-		links = UrlData.find_by_tutorial_id(tutorial_id).moz_backlinks.to_f + UrlData.find_by_tutorial_id(tutorial_id).google_backlinks.to_f
+		if UrlData.find_by_tutorial_id(tutorial_id).nil?
+			links = 0
+		else   
+			links = UrlData.find_by_tutorial_id(tutorial_id).moz_backlinks.to_f + UrlData.find_by_tutorial_id(tutorial_id).google_backlinks.to_f
+		end
 		course_popularity = tweets + links
 		course_popularity = course_popularity.to_i
 		return course_popularity
 	end
 
-	def popular_book(book_id, backlinks = @backlinks)
+	def popular_book(book_id)
 		tweets = Tweet.find_all_by_book_id(book_id).count                
-		links = UrlData.find_by_book_id(book_id).moz_backlinks.to_f + UrlData.find_by_book_id(book_id).google_backlinks.to_f
+		if UrlData.find_by_book_id(book_id).nil?
+			links = 0
+		else   
+			links = UrlData.find_by_book_id(book_id).moz_backlinks.to_f + UrlData.find_by_book_id(book_id).google_backlinks.to_f
+		end
 		course_popularity = tweets + links
 		course_popularity = course_popularity.to_i
 		return course_popularity
