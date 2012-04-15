a71876866ca96d125196e2ec0e549e2de0898bc3
carl sednaoui <carlsednaoui@gmail.com>
2012-04-15 01:44:12 -0400

    modified how course popularity was calculated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,27 @@ module ApplicationHelper
 		direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
 		link_to title, {:sort => column, :direction => direction}, {:class => css_class}
 	end
+
+	def popular_course(course_id, backlinks = @backlinks) 
+		tweets = Tweet.find_all_by_course_id(course_id).count
+		links = UrlData.find_by_course_id(course_id).moz_backlinks.to_f + UrlData.find_by_course_id(course_id).google_backlinks.to_f
+		course_popularity = tweets + links
+		course_popularity = course_popularity.to_i
+		return course_popularity
+	end
+
+	def all_backlinks
+		@data = UrlData.all
+
+		@backlinks = Array.new
+		@data.each do |details|
+			@backlinks.push details.google_backlinks
+			@backlinks.push details.moz_backlinks
+		end
+
+		@backlinks = @backlinks.compact.reject { |s| s.blank? }
+
+		@backlinks = @backlinks.inject(:+)
+		return @backlinks
+	end
 end
