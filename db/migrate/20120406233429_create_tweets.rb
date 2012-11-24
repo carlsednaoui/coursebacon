575134a03f4848c315cd45a5612c231bcd92a356
carl sednaoui <carlsednaoui@gmail.com>
2012-11-24 01:57:46 -0500

    reverted previous changes - it didnt work

--- db/migrate/20120406233429_create_tweets.rb
+++ db/migrate/20120406233429_create_tweets.rb
@@ -3,7 +3,7 @@ class CreateTweets < ActiveRecord::Migration
     create_table :tweets do |t|
       t.string :from_user
       t.string :tweet_text
-      t.column :tweet_id, :bigint, :limit => 8
+      t.column :tweet_id, :bigint
       t.integer :course_id
       t.integer :book_id
       t.integer :tutorial_id
