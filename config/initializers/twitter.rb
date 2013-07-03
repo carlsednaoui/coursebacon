995add9075b3de5d5dbf9cd24f20ff4f304f7c43
carl sednaoui <carlsednaoui@gmail.com>
2013-07-03 18:39:41 -0400

    Add Twitter gem initializer

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = 'kivBXFB5gE7RunUmBXgoWg'
+  config.consumer_secret = 'O8PLhMwETapHwD55AuUL4K8e27XwDb1GZI4jtj4cwg'
+  config.oauth_token = '292743631-0r47sb6Y1JqSAVafy0LAFTLyehtCb6U0nOt36SzH'
+  config.oauth_token_secret = 'ri6RBHquBq974i7wfllxroTn0QpRNUqhcthNa1Ct9Rk'
+end
